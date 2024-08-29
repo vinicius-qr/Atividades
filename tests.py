2daa23af949f0acc40b95d980ed2bee1539d0e8f
vinicius-qr <viniciusqr73@hotmail.com>
2024-08-29 08:12:43 -0300

    Add files via upload

---
+++ tests.py
@@ -0,0 +1,37 @@
+"""
+#Ex1
+cor = input('digite uma cor: ')
+num1 = input('digite um numero: ')
+num2 = input('digite um segundo numero: ')
+print(f'a sua cor é {cor}, e a sema dos seus numeros são: {int(num1)+ int(num2)}')
+"""
+
+"""
+#Ex2
+nome = input('digite um nome: ')
+nota = input('digite a sua nota: ')
+print(f'{nome.title()} tirou {nota}')
+"""
+
+"""
+#Ex3
+n1 = int(input('digite a sua nota: '))
+n2 = int(input('digite a sua nota: '))
+n3 = int(input('digite a sua nota: '))
+n4 = int(input('digite a sua nota: '))
+media = (n1+n2+n3+n4)/4
+print(f'a media das notas são {media}')
+"""
+
+"""
+#Ex4
+tempC = float(input('Temperatura em °C: '))
+tempF = tempC * 1.8 + 32
+print(f'A temperatura em °F é: {tempF}°F\n')
+
+tempC = float(input('Temperatura em °C: '))
+tempK = tempC + 273.15
+print(f'A temperatura em °K é: {tempK}°K')
+"""
+
+
